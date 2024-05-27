3eada026f88d8971fd9d23f64b91b87c34844df6
Mikakokeshi <mikakokeshi00124@gmail.com>
2024-05-27 15:30:05 +0900

    ✨ get playlist using API

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+
+## Deploy
+Vercel https://music-app-gamma-liard.vercel.app/
+
+
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 
-function App() {
+import { useEffect } from "react";
+import { SongList } from "./components/SongList";
+import spotify from "./lib/spotify";
+
+export default function App() {
+
+  // spotify.test()
+  useEffect(()=>{
+    spotify.getPopularSongs();
+  },[]);
+
   return (
-    <div className="App">
-Hello world
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      <main className="flex-1 p-8 mb-20">
+        <header className="flex justify-between items-center mb-10">
+          <h1 className="text-4xl font-bold">Music App</h1>
+        </header>
+        <section>
+          <h2 className="text-2xl font-semibold mb-5">Popular Songs</h2>
+          <SongList />
+        </section>
+      </main>
     </div>
   );
 }
-
-export default App;
